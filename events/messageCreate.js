e2b89c7084d21f45e0935612590bd8e02decdf62
Ferdinand <ferdinand.cruceru@gmail.com>
2022-05-20 14:23:45 +0100

    feat: add sending message to updates channel

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,26 @@
+const { updatesChannelId } = require("../config.json");
+
 module.exports = {
 	name: "messageCreate",
 	execute(message) {
 		console.log(
 			`${message.author} in #${message.channel.name} typed a message: ${message.content}.`,
 		);
+		if (message.content.includes("@MP")) {
+			let chapter = message.content.substring(
+				message.content.search(/chapter-\d{4}/) + 8,
+				message.content.length - 1,
+			);
+			console.log(`Got new MP chapter: ${chapter}`);
+
+			// Send message to correct channel
+			const updatesChannel =
+				message.client.channels.cache.get(updatesChannelId);
+			let url = message.content.substring(
+				message.content.search(/https?:\/\//),
+				message.content.length,
+			);
+			updatesChannel.send(`New MP Chapter **${chapter}** out: ${url}`);
+		}
 	},
 };
